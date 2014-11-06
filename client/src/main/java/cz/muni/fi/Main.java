547ba84d564cf67d725cf717cc5bf6982de3da27
edee111 <324681@mail.muni.cz>
2014-11-06 21:55:51 +0100

    Add client subproject

---
+++ client/src/main/java/cz/muni/fi/Main.java
@@ -0,0 +1,53 @@
+package cz.muni.fi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Eduard Tomek
+ * @since 6.11.14
+ */
+public class Main {
+  private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+
+    log.debug("Starting...");
+    ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext(new String[]{"application-context.xml"});
+    BeanFactory factory = (BeanFactory) appContext;
+
+    ExecutorService xrayExecutor = Executors.newSingleThreadExecutor();
+
+    xrayExecutor.submit(new Runnable() {
+      public void run() {
+        log.debug(getStartingMessage());
+
+        while (true) {
+
+
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException e) {
+            log.error("Thread Interrupted", e);
+          }
+        }
+
+      }
+    });
+  }
+
+
+  private static String getStartingMessage() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("\n\n************************************************************");
+    sb.append("\n* STARTING *");
+    sb.append("\n************************************************************\n");
+    return sb.toString();
+  }
+}
---
+++ client/src/main/java/cz/muni/fi/runtime/GlobalClient.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.runtime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Eduard Tomek
+ * @since 6.11.14
+ */
+public class GlobalClient {
+
+  public
+
+}
