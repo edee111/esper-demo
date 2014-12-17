1c6bae8b20e2e123b933d9bbf4e2b1876d605705
edee111 <324681@mail.muni.cz>
2014-12-17 12:05:48 +0100

    probe and client main classer rename

--- client/src/main/java/cz/muni/fi/ClientMain.java
+++ client/src/main/java/cz/muni/fi/ClientMain.java
@@ -1,23 +1,16 @@
 package cz.muni.fi;
 
-import cz.muni.fi.runtime.GlobalClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * @author Eduard Tomek
  * @since 6.11.14
  */
-public class Main {
-  private static final Logger log = LoggerFactory.getLogger(Main.class);
+public class ClientMain {
+  private static final Logger log = LoggerFactory.getLogger(ClientMain.class);
 
   public static void main(String[] args) {
 
--- probe/src/main/java/cz/muni/fi/ProbeMain.java
+++ probe/src/main/java/cz/muni/fi/ProbeMain.java
@@ -9,12 +9,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Entry point for the Demo. Run this from your IDE, or from the command line using 'mvn exec:java'.
  */
-public class StartDemo {
+public class ProbeMain {
 
   /**
    * Logger
    */
-  private static final Logger log = LoggerFactory.getLogger(StartDemo.class);
+  private static final Logger log = LoggerFactory.getLogger(ProbeMain.class);
 
 
   /**
